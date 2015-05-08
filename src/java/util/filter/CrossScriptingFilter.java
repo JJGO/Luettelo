738631a6f6f170ec7860956e4f0fa00c844463ec
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2015-05-08 18:11:19 +0200

    XSS protection ready for release
    Added a XSS filter to avoid getting scripting attacks

--- src/java/util/filter/CrossScriptingFilter.java
+++ src/java/util/filter/CrossScriptingFilter.java
@@ -1,5 +1,5 @@
 package util.filter;
- 
+ 
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,23 +8,25 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
- 
- 
+ 
+ 
 public class CrossScriptingFilter implements Filter {
- 
-    public void init(FilterConfig filterConfig) throws ServletException {
-        this.filterConfig = filterConfig;
-    }
- 
-    public void destroy() {
-        this.filterConfig = null;
-    }
- 
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-        throws IOException, ServletException {
- 
-        chain.doFilter(new RequestWrapper((HttpServletRequest) request), response);
- 
-    }
- 
+ 
+    private FilterConfig filterConfig;
+    
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+ 
+    public void destroy() {
+        this.filterConfig = null;
+    }
+ 
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+ 
+        chain.doFilter(new RequestWrapper((HttpServletRequest) request), response);
+ 
+    }
+ 
 }
